d5f2dc476d9a828377d47eb78a8ce1b77b142422
justizha <alfathizhaaaaaaa@gmail.com>
2024-02-05 16:40:47 +0700

    update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,21 @@
-<template>
-  <div>
-    <ul>
-      <li v-for="item in items" :key="item.id">{{ item.id }}</li>
-      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
-      <li v-for="item in items" :key="item.id">{{ item.email }}</li>
-    </ul>
-  </div>
-</template>
-
 <script>
-import axios from 'axios';
+import Nav from './components/Nav.vue'
+import Items from './components/Items.vue'
 
 export default {
-  data() {
-    return {
-      items: []
-    };
-  },
-  created() {
-    axios.get('http://127.0.0.1:8000/api/data')
-      .then(response => {
-        this.items = response.data.data; 
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }
+    name: 'App',
+    components: {
+        Items, Nav
+    }
 }
 </script>
+
+<template>
+    <main class="bg-gray-900 h-screen">
+        <Nav />
+        <section class="pt-20 mx-auto">
+            <Items />
+        </section>
+    </main>
+</template>
+
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "Nav"
+}
+</script>
+
+<template>
+    <nav class="bg-gray-800  w-full absolute py-2 top-0">
+        <div class="flex justify-between mx-4">
+            <h1 class="font-bold text-2xl text-white">
+                VUM
+            </h1>
+            <ul class="text-base text-white md:flex gap-8 hidden">
+                <li>
+                    <a href="#" class="hover:text-gray-200 hover:underline duration-200">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:text-gray-200 hover:underline duration-200">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
