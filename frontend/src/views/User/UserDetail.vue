464c972b1d75ec8ec205bc0fd4bf961e6f64099c
justizha <alfathizhaaaaaaa@gmail.com>
2024-02-16 09:06:15 +0700

    type declaration

--- frontend/src/views/User/UserDetail.vue
+++ frontend/src/views/User/UserDetail.vue
@@ -7,17 +7,22 @@
 </template>
 
 
-<script>
+<script lang="ts">
+interface User {
+    id: number,
+    name: string,
+    email: string
+}
     export default {
         data(){
             return {
-                user: null
+                user: null as User | null
             }
         },
         mounted() {
            fetch(`http://127.0.0.1:8000/api/user/${this.$route.params.id}`)
             .then(res => res.json())
-            .then(data => this.user = data)
+            .then((data : User) => this.user = data)
             .catch(err => console.error(err.message))
         }
     }
--- frontend/src/views/User/UserList.vue
+++ frontend/src/views/User/UserList.vue
@@ -1,61 +1,63 @@
+<script lang="ts">
+interface User {
+  id : number,
+  name : string,
+  email : string
+}
 
-<script>
 export default {
   name : 'UserList',
   data() {
     return {
-      users: []
+      users: [] as User[]
     }
   },
   mounted() {
     fetch('http://127.0.0.1:8000/api/users')
       .then(res => res.json())
-      .then(data => this.users = data)
+      .then((data: User[]) => this.users = data)
       .catch(err => console.error(err.message))
   }
 }
 </script>
 <template>
   <section class="pt-20">
-    
-
-<div class="flex justify-center">
-    <table class="w-full max-w-3xl  text-sm text-left  text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Id
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Name
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Email
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  Detail
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" v-for="user in users" :key="user.id">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  {{ user.id }}
-                </th>
-                <td class="px-6 py-4">
-                  {{ user.name }}
-                </td>
-                <td class="px-6 py-4">
-                  {{ user.email }}
-                </td>
-                <td class="px-6 py-4">
-                   <router-link :to="{ name: 'UserDetail', params: { id: user.id } }">Detail</router-link>
-                </td>
-            </tr>  
-        </tbody>
-    </table>
-</div>
-
+    <div class="flex justify-center">
+        <table class="w-full max-w-3xl  text-sm text-left  text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Id
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Email
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Detail
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" v-for="user in users" :key="user.id">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {{ user.id }}
+                    </th>
+                    <td class="px-6 py-4">
+                      {{ user.name }}
+                    </td>
+                    <td class="px-6 py-4">
+                      {{ user.email }}
+                    </td>
+                    <td class="px-6 py-4">
+                      <router-link :to="{ name: 'UserDetail', params: { id: user.id } }">Detail</router-link>
+                    </td>
+                </tr>  
+            </tbody>
+        </table>
+    </div>
   </section>
 </template>
 
