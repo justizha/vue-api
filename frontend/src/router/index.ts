8e08a487c34519592f06eac28553870bba2c5076
justizha <aflathizhaaaaaaa@gmail.com>
2024-02-27 11:31:26 +0700

    feat : home page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,11 +4,13 @@ import IndexProductVue from '@/views/Products/IndexProduct.vue';
 import UserCreateVue from '@/views/User/UserCreate.vue';
 import UserDetail from '@/views/User/UserDetail.vue';
 import UserListVue from '@/views/User/UserList.vue';
+import HomeIndexVue from '@/views/Home/HomeIndex.vue'
 
 
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
+  {path: '/' ,name :"HomeIndex", component : HomeIndexVue},
   {path: '/user',name : 'UserList',component: UserListVue},
   {path: '/user/:id', name: 'UserDetail', component: UserDetail},
   {path: '/user/create',name: 'UserCreate', component: UserCreateVue},
---
+++ frontend/src/views/Home/HomeIndex.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="relative mx-auto">
+        <div class="w-full grid grid-cols-4 gap-2">
+            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
+            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
+            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
+            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
+            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
+        </div>
+    </section>
+</template>
+
+
+<script>
+
+</script>
