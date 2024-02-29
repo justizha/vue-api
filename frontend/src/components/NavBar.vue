f42ef90a7351a6a993c83b0b5b923ad5d059f0af
justizha <aflathizhaaaaaaa@gmail.com>
2024-02-29 10:07:08 +0700

    style : hover effect fix : nav link

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,7 +2,7 @@
     <nav class="py-1 w-full  flex justify-between  bg-transparent">
         <div class="m-2">
             <h1 class="text-2xl font-bold dark:text-white">
-                Vue
+                <router-link to="/">Vue</router-link>
             </h1>
             <ul class="flex gap-3">
                 <li>
--- frontend/src/views/Home/HomeIndex.vue
+++ frontend/src/views/Home/HomeIndex.vue
@@ -2,18 +2,19 @@
     <section class="relative mx-auto py-24">
         <div class="flex justify-center">
             <div class="w-full grid grid-cols-1 gap-2 text-center max-w-2xl">
-                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                <router-link to="/user" class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300">
                     User View
                 </router-link>
-                <router-link to="/products" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                <router-link to="/products" class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300">
                     Product View
                 </router-link>
-                <router-link to="/" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                <router-link to="/" class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300">
                     Fake Store (Soon)
                 </router-link>
-                <router-link to="/" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
-                    Realtion (Soon)
+                <router-link to="/" class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300">
+                    Relation (Soon)
                 </router-link>
+                
             </div>
         </div>
     </section>
