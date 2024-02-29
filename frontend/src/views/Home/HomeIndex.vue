0e42cfdfa7b618e2b4137974aa06e072d461f515
justizha <aflathizhaaaaaaa@gmail.com>
2024-02-29 09:34:36 +0700

    fix : added new link title and fixing duplicated url

--- frontend/src/views/Home/HomeIndex.vue
+++ frontend/src/views/Home/HomeIndex.vue
@@ -3,19 +3,16 @@
         <div class="flex justify-center">
             <div class="w-full grid grid-cols-1 gap-2 text-center max-w-2xl">
                 <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
-                    Link
+                    User View
                 </router-link>
-                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
-                    Link
+                <router-link to="/products" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                    Product View
                 </router-link>
-                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
-                    Link
+                <router-link to="/" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                    Fake Store (Soon)
                 </router-link>
-                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
-                    Link
-                </router-link>
-                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
-                    Link
+                <router-link to="/" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                    Realtion (Soon)
                 </router-link>
             </div>
         </div>
