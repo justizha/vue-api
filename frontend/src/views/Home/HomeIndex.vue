96ef58ff9d6f88d4198ce6c690ede55de7c997d0
justizha <aflathizhaaaaaaa@gmail.com>
2024-02-29 09:30:34 +0700

    style : updated home page view

--- frontend/src/views/Home/HomeIndex.vue
+++ frontend/src/views/Home/HomeIndex.vue
@@ -1,11 +1,23 @@
 <template>
-    <section class="relative mx-auto">
-        <div class="w-full grid grid-cols-4 gap-2">
-            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
-            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
-            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
-            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
-            <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">Link</router-link>
+    <section class="relative mx-auto py-24">
+        <div class="flex justify-center">
+            <div class="w-full grid grid-cols-1 gap-2 text-center max-w-2xl">
+                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                    Link
+                </router-link>
+                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                    Link
+                </router-link>
+                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                    Link
+                </router-link>
+                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                    Link
+                </router-link>
+                <router-link to="/user" class="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-600  p-1 rounded font-medium text-white">
+                    Link
+                </router-link>
+            </div>
         </div>
     </section>
 </template>
