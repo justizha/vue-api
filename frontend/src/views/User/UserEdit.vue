b4c9ae12423a6c4fba42f78ba74c4d5b07e26450
justizha <aflathizhaaaaaaa@gmail.com>
2024-03-01 13:45:55 +0700

    fix : adjust and fixing edit method

--- frontend/src/views/User/UserEdit.vue
+++ frontend/src/views/User/UserEdit.vue
@@ -1,62 +1,85 @@
 <template>
-    <section class="relative mx-auto">
-        <h1 class="mb-6 mt-8 text-center dark:text-white font-semibold text-2xl">Edit User</h1>
-        <div class="flex justify-center">
-            <div class="p-2 dark:bg-gray-700/30 rounded-md flex  justify-center shadow w-full max-w-md py-12">
-                <form @submit.prevent="submitForm()" class="flex flex-col gap-2 dark:text-white">
-                    <div>
-                        <input placeholder="Name" type="text" id="name" v-model="formData.name" class="px-2 w-80 py-1 rounded-md bg-transparent dark:border border-gray-700 shadow">
-                    </div>
-                    <div>
-                        <input placeholder="Email" type="email" id="email" v-model="formData.email" class="px-2 w-80 py-1 rounded-md bg-transparent dark:border border-gray-700 shadow">
-                    </div>
-                    <div>
-                        <input placeholder="Password" type="password" id="password" v-model="formData.password" class="px-2 w-80 py-1 rounded-md bg-transparent dark:border border-gray-700 shadow">
-                    </div>
-                    <div class="grid ">
-                        <button type="submit" class="bg-blue-600 hover:bg-blue-600 px-5 py-1 rounded font-medium text-white">
-                            Update
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </section>
+  <section class="relative mx-auto">
+    <h1 class="mb-6 mt-8 text-center dark:text-white font-semibold text-2xl">Edit User</h1>
+    <div class="flex justify-center">
+      <div
+        class="p-2 dark:bg-gray-700/30 rounded-md flex justify-center shadow w-full max-w-md py-12"
+      >
+        <form @submit.prevent="submitForm()" class="flex flex-col gap-2 dark:text-white">
+          <div>
+            <input
+              placeholder="Name"
+              type="text"
+              id="name"
+              v-model="formData.name"
+              class="px-2 w-80 py-1 rounded-md bg-transparent dark:border border-gray-700 shadow"
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Email"
+              type="email"
+              id="email"
+              v-model="formData.email"
+              class="px-2 w-80 py-1 rounded-md bg-transparent dark:border border-gray-700 shadow"
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Password"
+              type="password"
+              id="password"
+              v-model="formData.password"
+              class="px-2 w-80 py-1 rounded-md bg-transparent dark:border border-gray-700 shadow"
+            />
+          </div>
+          <div class="grid">
+            <button
+              type="submit"
+              class="bg-blue-600 hover:bg-blue-600 px-5 py-1 rounded font-medium text-white"
+            >
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            formData: {
-                name: '',
-                email: '',
-                password: ''
-            }
-        };
-    },
-    methods: {
-        submitForm() {
-            const updatedUser = {
-                name: this.formData.name,
-                email: this.formData.email,
-                password: this.formData.password
-            };
-
-            fetch(`http://127.0.0.1:8000/api/user/update/${this.$route.params.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(updatedUser)
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('updated successfully');
-                this.$router.push('/user');
-            })
-            .catch(error => console.error(error));
-        }
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        password: ''
+      }
     }
-};
+  },
+  methods: {
+    submitForm() {
+      const updatedUser = {
+        name: this.formData.name,
+        email: this.formData.email,
+        password: this.formData.password
+      }
+
+      fetch(`http://127.0.0.1:8000/api/user/update/${this.$route.params.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedUser)
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('updated successfully', data)
+          this.$router.push('/user')
+        })
+        .catch((error) => console.error(error))
+    }
+  }
+}
 </script>
