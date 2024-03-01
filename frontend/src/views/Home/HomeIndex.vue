9dd999869fb596deab0f0af6d8398ac6ad046162
justizha <aflathizhaaaaaaa@gmail.com>
2024-03-01 14:01:07 +0700

    refactor : format & spacing

--- frontend/src/views/Home/HomeIndex.vue
+++ frontend/src/views/Home/HomeIndex.vue
@@ -1,25 +1,34 @@
 <template>
-    <section class="relative mx-auto py-24">
-        <div class="flex justify-center">
-            <div class="w-full grid grid-cols-1 gap-2 text-center max-w-2xl">
-                <router-link to="/user" class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300">
-                    User View
-                </router-link>
-                <router-link to="/products" class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300">
-                    Product View
-                </router-link>
-                <router-link to="/notyet" class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300">
-                    Fake Store (Soon)  
-                </router-link>
-                <router-link to="/notyet" class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300">
-                    Relation (Soon)
-                </router-link>                
-            </div>
-        </div>
-    </section>
+  <section class="relative mx-auto py-24">
+    <div class="flex justify-center">
+      <div class="w-full grid grid-cols-1 gap-2 text-center max-w-2xl">
+        <router-link
+          to="/user"
+          class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300"
+        >
+          User View
+        </router-link>
+        <router-link
+          to="/products"
+          class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300"
+        >
+          Product View
+        </router-link>
+        <router-link
+          to="/notyet"
+          class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300"
+        >
+          Fake Store (Soon)
+        </router-link>
+        <router-link
+          to="/notyet"
+          class="border bg-transparent border-blue-700 from-blue-800/30 via-blue-700 to-blue-600 p-1 rounded font-medium text-white hover:bg-gradient-to-l transition-all duration-300"
+        >
+          Relation (Soon)
+        </router-link>
+      </div>
+    </div>
+  </section>
 </template>
 
-
-<script>
-
-</script>
+<script></script>
--- frontend/src/views/User/UserList.vue
+++ frontend/src/views/User/UserList.vue
@@ -1,93 +1,104 @@
 <script>
-  export default {
-    name: 'UserList',
-    data(){
-      return {
-        users: []
-      };
-    },
-    mounted() {
-      this.fetchUsers();
+export default {
+  name: 'UserList',
+  data() {
+    return {
+      users: []
+    }
   },
-    methods: {
-      fetchUsers() {
-        fetch('http://127.0.0.1:8000/api/user')
-          .then(res => res.json())
-          .then((data) => this.users = data)
-          .catch(err => console.error(err.message));
-      },
-      deleteUser(userId){
-        if (confirm("Are you sure?")) {
-          fetch(`http://127.0.0.1:8000/api/user/delete/${userId}`, {
-            method: 'DELETE',
-          })
-          .then(res => {
+  mounted() {
+    this.fetchUsers()
+  },
+  methods: {
+    fetchUsers() {
+      fetch('http://127.0.0.1:8000/api/user')
+        .then((res) => res.json())
+        .then((data) => (this.users = data))
+        .catch((err) => console.error(err.message))
+    },
+    deleteUser(userId) {
+      if (confirm('Are you sure?')) {
+        fetch(`http://127.0.0.1:8000/api/user/delete/${userId}`, {
+          method: 'DELETE'
+        })
+          .then((res) => {
             if (!res.ok) {
-              throw new Error('Failed to delete user');
+              throw new Error('Failed to delete user')
             }
-            this.fetchUsers();
+            this.fetchUsers()
           })
-          .catch(error => {
-            console.error('Failed to delete user', error);
-          });
-        }
+          .catch((error) => {
+            console.error('Failed to delete user', error)
+          })
       }
     }
   }
+}
 </script>
 
-
-<template>                    
+<template>
   <section class="pt-20">
     <div class="my-2 flex justify-center">
-      <router-link to="/user/create" class="bg-blue-700 hover:bg-blue-800 px-6 py-1 rounded text-white font-medium">
+      <router-link
+        to="/user/create"
+        class="bg-blue-700 hover:bg-blue-800 px-6 py-1 rounded text-white font-medium"
+      >
         Add new User
       </router-link>
     </div>
     <div class="flex justify-center">
-        <table class="w-full max-w-3xl text-sm text-left  text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Id 
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Name
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Email
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      Detail
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      action
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="user in users" :key="user.id">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                      {{ user.id }}
-                    </th>
-                    <td class="px-6 py-4">
-                      {{ user.name }}
-                    </td>
-                    <td class="px-6 py-4">
-                      {{ user.email }}
-                    </td>
-                    <td class="px-6 py-4">
-                      <router-link :to="{ name: 'UserDetail', params: { id: user.id }}" class="bg-blue-600 hover:bg-blue-700 p-1 rounded font-medium text-white">Detail</router-link>
-                    </td>
-                    <td class="px-6 py-4">
-                      <a @click="deleteUser(user.id)" class="cursor-pointer px-2 py-1 rounded bg-red-500 font-medium text-white">Delete</a>
-                      <router-link :to="{ name: 'UserEdit', params: { id: user.id }}" class="bg-yellow-500  p-1 rounded font-medium text-white">Edit</router-link>
-                    </td>
-                </tr>  
-            </tbody>
-        </table>
+      <table class="w-full max-w-3xl text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead
+          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3">Id</th>
+            <th scope="col" class="px-6 py-3">Name</th>
+            <th scope="col" class="px-6 py-3">Email</th>
+            <th scope="col" class="px-6 py-3">Detail</th>
+            <th scope="col" class="px-6 py-3">action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+            v-for="user in users"
+            :key="user.id"
+          >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              {{ user.id }}
+            </th>
+            <td class="px-6 py-4">
+              {{ user.name }}
+            </td>
+            <td class="px-6 py-4">
+              {{ user.email }}
+            </td>
+            <td class="px-6 py-4">
+              <router-link
+                :to="{ name: 'UserDetail', params: { id: user.id } }"
+                class="bg-blue-600 hover:bg-blue-700 p-1 rounded font-medium text-white"
+                >Detail</router-link
+              >
+            </td>
+            <td class="px-6 py-4">
+              <a
+                @click="deleteUser(user.id)"
+                class="cursor-pointer px-2 py-1 rounded bg-red-500 font-medium text-white"
+                >Delete</a
+              >
+              <router-link
+                :to="{ name: 'UserEdit', params: { id: user.id } }"
+                class="bg-yellow-500 p-1 rounded font-medium text-white"
+                >Edit</router-link
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </section>
 </template>
-
-
