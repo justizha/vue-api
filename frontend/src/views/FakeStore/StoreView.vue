f283774e387f2c993ecd120ebbfd01a612aedf12
justizha <aflathizhaaaaaaa@gmail.com>
2024-03-04 11:31:21 +0700

    style : basic ui for products

--- frontend/src/views/FakeStore/StoreView.vue
+++ frontend/src/views/FakeStore/StoreView.vue
@@ -1,9 +1,30 @@
 <script lang="ts">
 export default {
-  name: 'StoreView'
+  name: 'StoreView',
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted() {
+    this.fetchProducts()
+  },
+  methods: {
+    fetchProducts() {
+      fetch('https://fakestoreapi.com/products')
+        .then((res) => res.json())
+        .then((data) => (this.products = data))
+        .catch((err) => console.error('Something went wrong :', err))
+    }
+  }
 }
 </script>
 <template>
-  <section></section>
+  <section class="mx-2">
+    <div class="py-20 grid grid-cols-5 gap-2">
+      <div class="grid grid-cols-2" v-for="product in products" :key="product.id">
+        <h1 class="text-white truncate">{{ product.title }}</h1>
+      </div>
+    </div>
+  </section>
 </template>
-./StoreView.vue
